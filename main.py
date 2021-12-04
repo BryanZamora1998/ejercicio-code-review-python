e3ed7164b2f2e8005b9fa73ac14831631c227961
Bryan Zamora <stevenzamora8@gmail.com>
2021-12-03 21:06:03 -0500

    Se procede a realizar desarrollo de claculadora

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from calculadora import calculadora
+
+
+def principal():
+    op=calculadora()
+
+principal()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
